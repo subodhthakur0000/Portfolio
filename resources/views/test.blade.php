0fefc1f18ad7373647ad53699ddb59c8e5464b9c
subodhthakur0000 <subodhthakur0000@gmail.com>
2019-11-24 12:44:41 +0545

    first phase

---
+++ resources/views/test.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>test particles</title>
+	<link rel="stylesheet" type="text/css" href="{{asset('public/frontend/particles/style.css')}}">
+</head>
+<body>
+	<div id="particles-js"></div>
+	<script src="{{asset('public/frontend/particles/particles.js')}}"></script>
+	<script src="{{asset('public/frontend/particles/app.js')}}"></script>
+
+
+</body>
+</html>
